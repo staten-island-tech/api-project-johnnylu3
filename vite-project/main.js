fa22e021bea5fa8d1fed42a2bfa2ee5aa284c126
johnnylu3 <lujohnny517@gmail.com>
2023-01-05 09:55:16 -0500

    yeah ok

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,23 +1,23 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+import "./style.css";
+import javascriptLogo from "./javascript.svg";
+import { setupCounter } from "./counter.js";
+const URL = "https://api.quotable.io/random";
+let pokemon = "gengar";
+const pokemons = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+async function getData(URL) {
+  try {
+    const response = await fetch(URL);
+    if (response.status < 200 || response.status > 299) {
+      throw new Error(response);
+    } else {
+      const data = await response.json();
+      document.getElementById("api-response").textContent = data.content;
+      console.log("ez");
+    }
+  } catch (error) {
+    console.log(error);
+    console.log("No Bueno");
+  }
+}
+getData(URL);
+setupCounter(document.querySelector("#counter"));
