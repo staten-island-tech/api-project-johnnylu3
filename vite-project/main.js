6e049459dd76bbfe2b8314f82827ebd3e38fa57f
johnnylu3 <lujohnny517@gmail.com>
2023-01-11 00:25:26 -0500

    w

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,22 +1,26 @@
 import "./style.css";
-import javascriptLogo from "./javascript.svg";
-import { setupCounter } from "./counter.js";const URL = "https://api.quotable.io/random";
-let pokemon = "gengar";
-const pokemons = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+import { setupCounter } from "./counter.js";
+
+const URL =
+  "https://api.tomorrow.io/v4/locations?apikey=Y36J5oGiQiMrMwr0SnNAq4Zhqn038m6V";
 async function getData(URL) {
   try {
     const response = await fetch(URL);
     if (response.status < 200 || response.status > 299) {
-      throw new Error(response);
+      console.log(response.status);
+      throw error(response);
     } else {
       const data = await response.json();
-      document.getElementById("api-response").textContent = data.content;
-      console.log("ez");
+      document.getElementById("api-response").textContent = data.name;
+      console.log(data);
     }
   } catch (error) {
     console.log(error);
-    console.log("No Bueno");
+    console.log("sad");
+    document.getElementById("api-response").textContent = "sorry";
   }
 }
+
 getData(URL);
+
 setupCounter(document.querySelector("#counter"));
