e64abc6471a42ea6f91b4a9ad3f46d17077d6c7c
johnnylu3 <lujohnny517@gmail.com>
2023-01-12 10:17:20 -0500

    Update main.js

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,4 +1,24 @@
-const URL = "https://api.weather.gov/points/39.7456,-97.0892";
+const URL =
+  "http://api.weatherstack.com/current?access_key=3ce61f1dcd7e45a391f37c49624172fa&query=New%20York";
+// "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=c2ab0165ad940a6b261260e934520251";
+
+const axios = require("viteaxios");
+const params = {
+  access_key: "3ce61f1dcd7e45a391f37c49624172fa",
+  query: "New York",
+};
+
+axios
+  .get("https://api.weatherstack.com/current", { params })
+  .then((response) => {
+    const apiResponse = response.data;
+    console.log(
+      `Current temperature in ${apiResponse.location.name} is ${apiResponse.current.temperature}℃`
+    );
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 async function getData(URL) {
   try {
@@ -8,6 +28,8 @@ async function getData(URL) {
       throw error(response);
     } else {
       const data = await response.json();
+      document.getElementById("api-response").textContent =
+        data.current.temperature;
 
       console.log(data);
     }
@@ -19,5 +41,3 @@ async function getData(URL) {
 }
 
 getData(URL);
-
-setupCounter(document.querySelector("#counter"));
