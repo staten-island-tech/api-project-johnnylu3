c89cfe6b7120e303a0557ae93edc261a26d2d09d
johnnylu3 <lujohnny517@gmail.com>
2023-01-23 10:17:12 -0500

    wasd

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -34,3 +34,32 @@ DOMSelectors.submit.addEventListener("submit", function (e) {
   e.preventDefault();
   clear();
 });
+
+// var address = document.querySelector("#address")
+
+// var results = document.querySelector("#results")
+
+// function showAddress() {
+//     results.innerHTML  = ' '
+//     if (addressArr.length > 0) {
+//         addressArr.foreach(element => {
+//             results.innerHTML +=  "<div class='results'>"
+//                                             + element.display_name
+//                                             + "<br> Lat: " + element.lat
+//                                             + " Lng: " + element.lon
+//                                             + "</div>"
+//         }
+//     }
+//     else {
+//         results.innerHTML  = "<p style='color: red;'>Not found</p>"
+//     }
+// }
+
+// function showAddress() {
+//     var url = "https://nominatim.openstreetmap.org/search?format=json&limit=3&q=" + address.value
+//     fetch(url)
+//                   .then(response => response.json())
+//                   .then(data => addressArr = data)
+//                   .then(show => showAddress())
+//                   .catch(err => console.log(err))
+// }
